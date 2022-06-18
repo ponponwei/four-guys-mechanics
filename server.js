5827a0f468bc1e957e2f8a6ef5bdd0ce6bb933cc
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-06-18 11:06:40 -0400

    added code to server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers/appointment-routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.envPORT || 3001;
+
+const hbs = exphbs.create({});
+console.log(hbs.handlebars);
+hbs.handlebars.logger.level = 'info';
+console.log('Current log level: ', hbs.handlebars.logger.level, '\n---');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/appointment-routes'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening to: http://localhost:${PORT}`));
+})
