c01897880446e1c0ad1ef630e73dd097659dbbdb
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-06-18 13:57:16 -0400

    added connection.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
 const appointmentRoutes = require('./appointmentRoutes');
 
 router.use('/user', userRoutes);
