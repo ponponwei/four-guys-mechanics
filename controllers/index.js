a5bc134b3dd9bbdfe77fd7372dcda3fe874ed496
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-06-22 21:19:50 -0400

    rm withAuth from index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-router.use('/api', withAuth, apiRoutes);
-router.use('/', withAuth, homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
